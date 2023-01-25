c586b381802527af0c0cb2811319710d30c385f3
Andrey Muller <www.mel77@mail.ru>
2023-01-26 02:11:27 +0300

    start api

--- js/main.js
+++ js/main.js
@@ -161,6 +161,11 @@ let gameStart = false
 let gameEnd = false
 
 window.onload = async function () {
+    VK.init(function() {
+        console.log('vk init complete')
+    }, function() {
+        console.log('error')
+    }, '5.131');
     const app = new PIXI.Application({
         width: gameWidth,
         height: gameHeight,
