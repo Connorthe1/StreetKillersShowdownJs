43db54fa50914d58f749b388efa5f1e2eaf09660
Andrey Melnik <www.mel77@mail.ru>
2026-07-02 03:29:17 +0300

    test

--- src/js/storage/StorageManager.js
+++ src/js/storage/StorageManager.js
@@ -6,7 +6,7 @@ export const BASE_STORAGE = {
     muteSound: false,
     muteMusic: false,
     lastUpdate: 0,
-    selectedSkin: 3,
+    selectedSkin: 0,
     ownedSkins: [0],
     activeItems: {
         stimpack: 0,
